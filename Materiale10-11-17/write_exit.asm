afea8fc513412ebe7b2e68640372279756bb1a7f
HAQLAB <32841034+HAQLAB@users.noreply.github.com>
2017-11-10 19:01:43 +0100

    Create write_exit.asm

---
+++ Materiale10-11-17/write_exit.asm
@@ -0,0 +1,29 @@
+bits 32
+SECTION .text
+global _start
+
+_start:
+  ;azzeriamo i registri
+  xor eax, eax
+  xor ebx, ebx
+  xor ecx, ecx
+  xor edx, edx
+                         ;eax   ebx        ecx              edx
+  ;eseguiamo la syscall write(int fd, const void *buf, size_t count)
+  mov bl, 0x1       ;standard output
+  push cl           ;pushiamo uno zero sulla stack
+  push 0x44434241   ;pushiamo la stringa ABCD(little-endian) sullo stack
+  mov ecx, esp      ;mettiamo in ecx l'indirizzo del top dello stack (ovvero della stringa ABCD)
+  mov al, 0x4       ;mettiamo negli ultimi 8 bit di eax il valore della syscall write (cat /usr/include/x86_64-linux-gnu/asm/unistd_32.h|grep write)
+  
+  int 0x80          ;chiamiamo un interrupt che fa eseguire la syscall
+  
+  ;azzeriamo i registri
+  xor eax, eax
+  xor ebx, ebx
+  xor ecx, ecx
+  xor edx, edx
+  
+  ;eseguiamo la syscall exit(0)
+  mov al, 0x1
+  int 0x80
